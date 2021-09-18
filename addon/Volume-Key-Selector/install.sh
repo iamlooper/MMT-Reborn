98d120de6131703d66a9f67e7c4451335254f5bf
anylooper <iaminfinitylooper@gmail.com>
2021-09-18 10:49:04 +0500

    Fixed vol key issues

--- addon/Volume-Key-Selector/install.sh
+++ addon/Volume-Key-Selector/install.sh
@@ -1,53 +1,53 @@
 # External Tools
 
-chmod -R 0755 "$MODPATH/addon/Volume-Key-Selector/tools"
+chmod -R 0755 "$TMPDIR/addon/Volume-Key-Selector/tools"
 export PATH=$TMPDIR/addon/Volume-Key-Selector/tools/$ARCH32:$PATH
 
-function keytest() {
+keytest(){
   ui_print "[*] Vol Key Test"
   ui_print "[*] Press a Vol Key: "
-  if (timeout 9 /system/bin/getevent -lc 1 2>&1 | /system/bin/grep "VOLUME" | /system/bin/grep "DOWN" > $TMPDIR/events); then
+  if $(timeout 9 /system/bin/getevent -lc 1 2>&1 | /system/bin/grep "VOLUME" | /system/bin/grep "DOWN" > $TMPDIR/events); then
     return 0
   else
     ui_print "[*] Try again:"
     timeout 9 keycheck
     local SEL=$?
-    [ $SEL -eq 143 ] && abort "[!] Vol key not detected!" || return 1
+    [ "$SEL" = "143" ] && abort "[!] Vol key not detected!" || return 1
   fi
 }
 
-function chooseport() {
+chooseport(){
   # Original idea by chainfire @xda-developers, improved on by ianmacd @xda-developers
   #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
   while true; do
     /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $TMPDIR/events
-    if (`cat $TMPDIR/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+    if $(cat $TMPDIR/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null); then
       break
     fi
   done
-  if (`cat $TMPDIR/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+  if $(cat $TMPDIR/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null); then
     return 0
   else
     return 1
   fi
 }
 
-function chooseportold() {
+chooseportold(){
   # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
   # Calling it first time detects previous input. Calling it second time will do what we want
   while true; do
     keycheck
     keycheck
     local SEL=$?
-    if [ "$1" == "UP" ]; then
+    if [ "$1" = "UP" ]; then
       UP=$SEL
       break
-    elif [ "$1" == "DOWN" ]; then
+    elif [ "$1" = "DOWN" ]; then
       DOWN=$SEL
       break
-    elif [ $SEL -eq $UP ]; then
+    elif [ "$SEL" = "$UP" ]; then
       return 0
-    elif [ $SEL -eq $DOWN ]; then
+    elif [ "$SEL" = "$DOWN" ]; then
       return 1
     fi
   done
