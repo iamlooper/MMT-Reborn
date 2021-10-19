7553a02d6a83a17101dbd8c23ec1e87a4b3f62de
anylooper <iaminfinitylooper@gmail.com>
2021-10-19 13:41:15 +0500

    Updated v1.1

--- setup.sh
+++ setup.sh
@@ -1,94 +1,71 @@
 #!/sbin/sh
 ###########################
-# MMT - BOURNE SETUP SCRIPT
+# MMT Bourne Setup Script
 ###########################
 
-# CONFIG VARS
-# Choose if you want to skip mount for your module or not.
-SKIPMOUNT=false
-# Set true if you want to clean old files in module before injecting new module
+############
+# Config Vars
+############
+
+# Uncomment if you want to skip mount for your module
+# Uncomment if you want to clean old files in module before injecting new module
+# Uncomment if you want to load vskel after module info print. If you want to manually load it, consider using load_vksel function
+# Uncomment DEBUG if you want full debug logs (Saved To Internal Storage)
+#SKIPMOUNT=true
 CLEANSERVICE=true
-# Select true if you want to want to debug
+#AUTOVKSEL=true
 DEBUG=true
 
-##############################
-# REPLACE
-##############################
+############
+# Replace List
+############
 
 # List all directories you want to directly replace in the system
-
-# Construct your list in the following format
-# This is an example
+# Construct your list in the following example format
 REPLACE_EXAMPLE="
 /system/app/Youtube
 /system/priv-app/SystemUI
 /system/priv-app/Settings
 /system/framework
 "
-
 # Construct your own list here
 REPLACE="
 "
 
-##############################
-# PERMISSIONS
-##############################
+############
+# Cleanup
+############
 
-# Set permissions (default perms are given by default)
-set_permissions(){
-#set_perm_recursive "$MODPATH/system/bin" 0 0 0755 0755
-}
+# Remove unnecessary stuff
+#do_cleanup(){
+#rm -rf "$MODPATH/LICENSE" 2>/dev/null
+#rm -rf "$MODPATH/README.md" 2>/dev/null
+#}
 
-##############################
-# INFO PRINT
-##############################
+############
+# Permissions
+############
 
-# Set what you want to be displayed on header on installation process (important)
-info_print(){
+# Set permissions
+#set_permissions(){
+#set_perm_recursive "$MODPATH" 0 0 0755 0644
+#}
+
+############
+# Info Print
+############
+
+# Set what you want to be displayed on header of installation process
+#info_print(){
 #awk '{print}' "$MODPATH/banner"
-ui_print " "
-}
+#ui_print ""
+#}
 
-##############################
-# MAIN
-##############################
+############
+# Main
+############
 
-# Change the logic to whatever you want.
-init_main(){
-
-# Change to whatever you want.
-ui_print " "
-
-# Load vksel
-load_vksel
-
-# Start vksel
-VK=1
-while true
-do
-ui_print "   $VK"
-if $VKSEL 
-then
-VK=$((VK + 1))
-else 
-break
-fi
-if [ $VK -gt 5 ]
-then
-VK=1
-fi
-done
-
-case $SM in
-1 ) FCTEXTAD1="Text";;
-esac
-
-ui_print "[*] Selected: $FCTEXTAD1 "
-
-if [ "$FCTEXTAD1" = "Text" ]; then
-fi
-
-# Change to whatever you want.
-ui_print " "
-
-}
+# Change the logic to whatever you want
+#init_main(){
+#ui_print ""
+#}
